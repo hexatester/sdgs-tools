5f1c54f38bf467058a30861b01ba730469a388a6
hexatester <revolusi147id@gmail.com>
2021-08-14 01:58:55 +0700

    Prepare individu

---
+++ sdgs_tools/aplikasi_sdgs/individu/__init__.py
@@ -0,0 +1,17 @@
+from uiautomator2 import Device
+from openpyxl.worksheet.worksheet import Worksheet
+
+
+ROW_PENGHASILAN = 2
+
+
+def get_survey_individu(
+    d: Device,
+    ws: Worksheet,
+    row: int,
+    rt: str,
+    rw: str,
+    no_kk: str,
+    nik: str,
+):
+    pass
