4bc2a3d8ee02dfa685c2b9026b94cd453d3be0d8
hexatester <revolusi147id@gmail.com>
2021-08-14 23:53:57 +0700

    Initial get_data_tenaga_kesehatan

--- sdgs_tools/aplikasi_sdgs/keluarga/__init__.py
+++ sdgs_tools/aplikasi_sdgs/keluarga/__init__.py
@@ -1,9 +1,11 @@
 from .lokasi import get_data_lokasi
 from .pendidikan import get_data_pendidikan
 from .kesehatan import get_data_kesehatan
+from .tenaga_kesehatan import get_data_tenaga_kesehatan
 
 __all__ = [
     "get_data_lokasi",
     "get_data_pendidikan",
     "get_data_kesehatan",
+    "get_data_tenaga_kesehatan"
 ]
