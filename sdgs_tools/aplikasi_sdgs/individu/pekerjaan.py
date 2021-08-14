3af61d1b833f1f987e0148a68dbc3f6fadb9a855
hexatester <revolusi147id@gmail.com>
2021-08-14 15:00:51 +0700

    get_data_pekerjaan

--- sdgs_tools/aplikasi_sdgs/individu/pekerjaan.py
+++ sdgs_tools/aplikasi_sdgs/individu/pekerjaan.py
@@ -1,6 +1,8 @@
 from uiautomator2 import Device
 from openpyxl.worksheet.worksheet import Worksheet
 
+from sdgs_tools.aplikasi_sdgs.utils import d_get_text
+
 # EXCEL COL : d(resourceId="{}")
 PEKERJAAN_COL = {
     "W": "com.kemendes.survey:id/cbKondisiPekerjaan",
@@ -10,5 +12,7 @@ PEKERJAAN_COL = {
 }
 
 
-def get_pekerjaan_individu(d: Device, ws: Worksheet, row: int):
+def get_data_pekerjaan(d: Device, ws: Worksheet, row: int):
     d(text="PEKERJAAN").click()
+    for col, resourceId in PEKERJAAN_COL.items():
+        ws[f"{col}{row}"] = d_get_text(d, resourceId)
