777e74bf9b2c1b64818de04b1d3268de0d777e68
hexatester <revolusi147id@gmail.com>
2021-08-14 15:03:07 +0700

    Improve utils

--- sdgs_tools/aplikasi_sdgs/utils.py
+++ sdgs_tools/aplikasi_sdgs/utils.py
@@ -1,10 +1,21 @@
 from openpyxl.worksheet.worksheet import Worksheet
 from typing import Dict, Optional
-from uiautomator2 import UiObject
+from uiautomator2 import Device, UiObject
 
 
 def get_text(ui: UiObject) -> Optional[str]:
+    childText = ui.child(className="android.widget.TextView")
+    if childText.exists:
+        return childText.info.get("text")
     return ui.info.get("text")
+
+
+def d_get_text(d: Device, resourceId: str):
+    if resourceId.startswith("com.kemendes.survey:id/txt"):
+        return d(resourceId=resourceId).info.get("text")
+    parent = d(resourceId=resourceId)
+    childText = parent.child(className="android.widget.TextView")
+    return childText.info.get("text")
 
 
 def set_ws_header(ws: Worksheet, header: Dict[str, str], row: int = 1):
