57286fe750e31582384761bb9c71c692c1fffa6f
hexatester <revolusi147id@gmail.com>
2021-08-14 15:03:22 +0700

    Initial export_individu command

--- sdgs_tools/cli/aplikasi.py
+++ sdgs_tools/cli/aplikasi.py
@@ -1,6 +1,8 @@
 import click
+from typing import Optional
 
-from sdgs_tools.aplikasi_sdgs.excel import make_template
+from sdgs_tools.aplikasi_sdgs.excel import make_template_individu
+from sdgs_tools.aplikasi_sdgs.export import export_individu as _export_individu
 
 
 @click.group("aplikasi")
@@ -9,10 +11,25 @@ def aplikasi():
 
 
 @aplikasi.command("template")
-@click.argument("nama_file", type=click.Path(), default="Template SDGS.xlsx")
+@click.argument("nama_file", type=click.Path(), default="Data INDIVIDU SDGS.xlsx")
 def template(nama_file: str):
     try:
-        make_template(filepath=nama_file)
+        make_template_individu(filepath=nama_file)
         click.echo(f"Berhasil membuat template")
     except Exception as e:
         click.echo(f"Gagal membuat template karena {repr(e)}")
+
+
+@aplikasi.command("export-individu")
+@click.option("--ranges", type=str, required=False)
+@click.option("--penghasilan", type=int, default=2, help="Baris kosong penghasilan")
+@click.argument("nama_file", type=click.Path(), default="Data INDIVIDU SDGS.xlsx")
+def export_individu(nama_file: str, ranges: Optional[str] = None, penghasilan: int = 2):
+    try:
+        _export_individu(
+            filepath=nama_file,
+            ranges=ranges,
+            row_penghasilan=penghasilan,
+        )
+    except Exception as e:
+        click.echo(f"Gagal mengeksport data individu karena {repr(e)}")
