966a33999b7f0c691d86aad3ea23f458c18f371d
hexatester <revolusi147id@gmail.com>
2021-08-14 15:01:01 +0700

    get_data_pendidikan

--- sdgs_tools/aplikasi_sdgs/individu/pendidikan.py
+++ sdgs_tools/aplikasi_sdgs/individu/pendidikan.py
@@ -1,6 +1,27 @@
 from uiautomator2 import Device
 from openpyxl.worksheet.worksheet import Worksheet
 
+from sdgs_tools.aplikasi_sdgs.utils import d_get_text
 
-def get_pendidikan_individu(d: Device, ws: Worksheet, row: int):
-    pass
+# resourceId
+PENDIDIKAN_COL = {
+    "BV": "com.kemendes.survey:id/cbPendidikanTertinggi",
+    "BW": "com.kemendes.survey:id/txtTahunPendidikanDasar",
+    "BX": "com.kemendes.survey:id/txtBahasa",
+    "BY": "com.kemendes.survey:id/txtBahasaLembaga",
+    "BZ": "com.kemendes.survey:id/txtKerjaBakti",
+    "CA": "com.kemendes.survey:id/txtSiskamling",
+    "CB": "com.kemendes.survey:id/txtPestaRakyat",
+    "CC": "com.kemendes.survey:id/txtMenolongKematian",
+    "CD": "com.kemendes.survey:id/txtMenolongSakit",
+    "CE": "com.kemendes.survey:id/txtMenolongKecelakaan",
+    "CF": "com.kemendes.survey:id/cbPerolehPelayananDesa",
+    "CG": "com.kemendes.survey:id/cbMasukan",
+    "CH": "com.kemendes.survey:id/cbBencana",
+}
+
+
+def get_data_pendidikan(d: Device, ws: Worksheet, row: int):
+    d(text="PENDIDIKAN").click()
+    for col, resourceId in PENDIDIKAN_COL.items():
+        ws[f"{col}{row}"] = d_get_text(d, resourceId)
