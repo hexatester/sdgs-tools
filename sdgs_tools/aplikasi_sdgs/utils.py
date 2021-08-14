35f64fadbd3bfe6a1a8e9a38aa21395aa9ca2f8d
hexatester <revolusi147id@gmail.com>
2021-08-14 11:37:05 +0700

    add_header

--- sdgs_tools/aplikasi_sdgs/utils.py
+++ sdgs_tools/aplikasi_sdgs/utils.py
@@ -1,6 +1,12 @@
-from typing import Optional
+from openpyxl.worksheet.worksheet import Worksheet
+from typing import Dict, Optional
 from uiautomator2 import UiObject
 
 
 def get_text(ui: UiObject) -> Optional[str]:
     return ui.info.get("text")
+
+
+def set_ws_header(ws: Worksheet, header: Dict[str, str], row: int = 1):
+    for col, nama in header.items():
+        ws[f"{col}{row}"] = nama.title()
