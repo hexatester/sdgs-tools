b570f2e5c6c2bd3b7e57c0794ec440e3c2d01fc2
hexatester <revolusi147id@gmail.com>
2021-08-14 15:02:16 +0700

    Update get_survey_individu

--- sdgs_tools/aplikasi_sdgs/individu/__init__.py
+++ sdgs_tools/aplikasi_sdgs/individu/__init__.py
@@ -1,17 +1,37 @@
 from uiautomator2 import Device
-from openpyxl.worksheet.worksheet import Worksheet
+from typing import List
+from openpyxl import Workbook
 
-
-ROW_PENGHASILAN = 2
+from .individu import get_data_individu
+from .pekerjaan import get_data_pekerjaan
+from .penghasilan import get_data_penghasilan
+from .kesehatan import get_data_kesehatan
+from .disabilitas import get_data_disabilitas
+from .pendidikan import get_data_pendidikan
 
 
 def get_survey_individu(
     d: Device,
-    ws: Worksheet,
+    wb: Workbook,
     row: int,
     rt: str,
     rw: str,
     no_kk: str,
     nik: str,
+    row_penghasilan=2,
 ):
-    pass
+    form_rt_rw = d(resourceId="com.kemendes.survey:id/txtRTRW")
+    form_rt_rw.send_keys(f"{rt}/{rw}", clear=True)
+    form_kk = d(resourceId="com.kemendes.survey:id/txtNoKK")
+    form_kk.send_keys(no_kk, clear=True)
+    form_nik = d(resourceId="com.kemendes.survey:id/txtNIK")
+    form_nik.send_keys(no_kk, clear=True)
+    individu = get_data_individu(d, wb["Individu"], row)
+    pekerjaan = get_data_pekerjaan(d, wb["Individu"], row)
+    row_penghasilan = get_data_penghasilan(
+        d, wb["Penghasilan"], nik, row_penghasilan
+    )
+    kesehatan = get_data_kesehatan(d, wb["Individu"], row)
+    disabilitas = get_data_disabilitas(d, wb["Individu"], row)
+    pendidikan = get_data_pendidikan(d, wb["Individu"], row)
+    return row_penghasilan
