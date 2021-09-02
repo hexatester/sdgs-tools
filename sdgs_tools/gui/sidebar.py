dbdf60195fd4cd02d232f663a61d04e1c3184962
hexatester <revolusi147id@gmail.com>
2021-09-02 21:10:16 +0700

    Initial add_button

--- sdgs_tools/gui/sidebar.py
+++ sdgs_tools/gui/sidebar.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from typing import Callable, Dict
 
 
 class SidebarFrame(tk.Frame):
@@ -13,3 +14,11 @@ class SidebarFrame(tk.Frame):
             **kw
         )
         self.pack(expand=False, fill="both", side="left", anchor="nw")
+        self.title = tk.Label(self, text="SDGS Tools")
+        self.title.pack()
+        self.buttons: Dict[str, tk.Button] = dict()
+
+    def add_button(self, text: str, command: Callable[[], None]):
+        button = tk.Button(self, text=text, command=command)
+        self.buttons[text] = button
+        button.pack()
