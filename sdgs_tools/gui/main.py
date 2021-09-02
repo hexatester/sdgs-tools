4a26459281d97c737a71b0b6b66469eb880c6af6
hexatester <revolusi147id@gmail.com>
2021-09-02 21:14:36 +0700

    Tentang button

--- sdgs_tools/gui/main.py
+++ sdgs_tools/gui/main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+from tkinter.messagebox import showinfo
 
+from sdgs_tools import __version__
 from . import ContentFrame
 from . import SidebarFrame
 
@@ -7,8 +9,17 @@ from . import SidebarFrame
 class MainApp(tk.Tk):
     def __init__(self):
         super().__init__(screenName="SDGS Tools")
+        self.title("SDGS Tools")
         self.sidebar = SidebarFrame(self)
         self.content = ContentFrame(self)
+        self.sidebar.add_button("Tentang", self.about)
 
     def __call__(self):
         self.mainloop()
+
+    def about(self):
+        showinfo(
+            "Tentang",
+            f"SDGS-Tools versi{__version__} dibuat oleh Habib Rohman\n"
+            "Donasi : https://saweria.co/hexatester",
+        )
