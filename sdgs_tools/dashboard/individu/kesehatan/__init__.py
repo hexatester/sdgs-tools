245661dcb48cd374cec5bf3bae4fc5955c9a267a
hexatester <revolusi147id@gmail.com>
2021-09-02 19:48:38 +0700

    Update DataKesehatan

--- sdgs_tools/dashboard/individu/kesehatan/__init__.py
+++ sdgs_tools/dashboard/individu/kesehatan/__init__.py
@@ -1,3 +1,4 @@
+import cattr
 from typing import Optional
 
 from .disabilitas import Disabilitas
@@ -7,28 +8,31 @@ from .penyakit_diderita import PenyakitDiderita
 
 __all__ = ["Disabilitas", "FasilitasKesehatan", "PenyakitDiderita"]
 
+cattr.unstructure(Disabilitas, Disabilitas.todict)
+cattr.unstructure(FasilitasKesehatan, FasilitasKesehatan.todict)
+cattr.unstructure(PenyakitDiderita, PenyakitDiderita.todict)
+
 
 class DataKesehatan:
     penyakit_diderita: PenyakitDiderita
     fasilitas_kesehatan: FasilitasKesehatan
-    jamsos_ketenagakerjaan: str
+    jamsos_kesehatan: str
     disabilitas: Disabilitas
     setahun_melahirkan: Optional[bool]
     mendapat_asi: Optional[bool]
 
     def todict(self):
-        # TODO UPDATE P401 & P404
         data = {
-            "I.P401": self.penyakit_diderita.todict(),
-            "I.P402": self.fasilitas_kesehatan.todict(),
-            "I.P403": self.jamsos_ketenagakerjaan,
-            "I.P404": self.disabilitas.todict(),
+            "I.P401": cattr.unstructure(self.penyakit_diderita),
+            "I.P402": cattr.unstructure(self.fasilitas_kesehatan),
+            "I.P403": self.jamsos_kesehatan,
+            "I.P404": cattr.unstructure(self.disabilitas),
             # "I.P405": "setahun_melahirkan",
             # "I.P406": "mendapat_asi",
         }
 
-        if self.setahun_melahirkan:
-            data["I.P405"] = self.setahun_melahirkan
-        if self.mendapat_asi:
-            data["I.P406"] = self.mendapat_asi
+        if self.setahun_melahirkan is not None:
+            data["I.P405"] = "1" if self.setahun_melahirkan else "2"
+        if self.mendapat_asi is not None:
+            data["I.P406"] = "1" if self.mendapat_asi else "2"
         return data
