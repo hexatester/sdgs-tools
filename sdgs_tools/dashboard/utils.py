88eb1a0f635d823ac7118145666e248ac993b454
hexatester <revolusi147id@gmail.com>
2021-09-04 20:33:45 +0700

    Added register_cattr_hooks

--- sdgs_tools/dashboard/utils.py
+++ sdgs_tools/dashboard/utils.py
@@ -1,5 +1,10 @@
+import cattr
 from datetime import date
 
 
 def date_to_str(value: date) -> str:
     return value.strftime("%Y-%m-%d")
+
+
+def register_cattr_hooks():
+    cattr.register_structure_hook(date, date_to_str)
