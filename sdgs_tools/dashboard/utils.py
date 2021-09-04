891e7cbe9d3953ea3507045be49625651a392baf
hexatester <revolusi147id@gmail.com>
2021-09-04 20:47:35 +0700

    Update hooks

--- sdgs_tools/dashboard/utils.py
+++ sdgs_tools/dashboard/utils.py
@@ -1,10 +1,18 @@
 import cattr
-from datetime import date
+from datetime import date, datetime
+from dateutil import parser
 
 
 def date_to_str(value: date) -> str:
     return value.strftime("%Y-%m-%d")
 
 
+def parse_datetime(text: str, t) -> datetime:
+    return parser.parse(
+        timestr=text,
+        parserinfo=parser.parserinfo(dayfirst=False, yearfirst=True),
+    )
+
+
 def register_cattr_hooks():
-    cattr.register_unstructure_hook(date, date_to_str)
+    cattr.register_structure_hook(datetime, parse_datetime)
