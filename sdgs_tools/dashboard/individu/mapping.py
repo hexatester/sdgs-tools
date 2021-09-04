04d6bb3a66ecac6e2750a2d1996506d4ba00d607
hexatester <revolusi147id@gmail.com>
2021-09-04 17:56:11 +0700

    Initial MappingIndividu.make_row

--- sdgs_tools/dashboard/individu/mapping.py
+++ sdgs_tools/dashboard/individu/mapping.py
@@ -1,4 +1,6 @@
 import attr
+from openpyxl.worksheet.worksheet import Worksheet
+from typing import Any, Dict
 
 MAPPING = {
     "desa": "desa",
@@ -83,3 +85,29 @@ class MappingIndividu:
     alamat_facebook: str = "T"
     alamat_twitter: str = "U"
     alamat_instagram: str = "V"
+
+    def make_row(self, ws: Worksheet, row: int) -> Dict[str, Any]:
+        return {
+            "no_kk": ws[f"{self.no_kk}{row}"],
+            "nik": ws[f"{self.nik}{row}"],
+            "nama": ws[f"{self.nama}{row}"],
+            "jenis_kelamin": ws[f"{self.jenis_kelamin}{row}"],
+            "tempat_lahir": ws[f"{self.tempat_lahir}{row}"],
+            "tanggal_lahir": ws[f"{self.tanggal_lahir}{row}"],
+            "usia": ws[f"{self.usia}{row}"],
+            "status_pernikahan": ws[f"{self.status_pernikahan}{row}"],
+            "agama": ws[f"{self.agama}{row}"],
+            "suku": ws[f"{self.suku}{row}"],
+            "warganegara": ws[f"{self.warganegara}{row}"],
+            "nomor_hp": ws[f"{self.nomor_hp}{row}"],
+            "aktif_internet": ws[f"{self.aktif_internet}{row}"],
+            "kecepatan_internet": ws[f"{self.kecepatan_internet}{row}"],
+            "agama_comment": ws[f"{self.agama_comment}{row}"],
+            "akses_melalui": ws[f"{self.akses_melalui}{row}"],
+            "usia_menikah": ws[f"{self.usia_menikah}{row}"],
+            "nomor_whatsapp": ws[f"{self.nomor_whatsapp}{row}"],
+            "alamat_email": ws[f"{self.alamat_email}{row}"],
+            "alamat_facebook": ws[f"{self.alamat_facebook}{row}"],
+            "alamat_twitter": ws[f"{self.alamat_twitter}{row}"],
+            "alamat_instagram": ws[f"{self.alamat_instagram}{row}"],
+        }
