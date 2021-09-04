e62f776dc3077084fab831e7117f7d354ffe29ae
hexatester <revolusi147id@gmail.com>
2021-09-04 22:01:24 +0700

    Update Diekspor

--- sdgs_tools/dashboard/enums/diekspor.py
+++ sdgs_tools/dashboard/enums/diekspor.py
@@ -2,15 +2,15 @@ from enum import Enum
 from . import make_str_to_enum
 
 MAPPING = {
-    "Diekspor": "1",
-    "Sebagian": "2",
+    "Semua": "1",
+    "Sebagian Besar": "2",
     "Tidak": "3",
 }
 
 
 class Diekspor(Enum):
-    DIEKSPOR = "1"
-    SEBAGIAN = "2"
+    SEMUA = "1"
+    SEBAGIAN_BESAR = "2"
     TIDAK = "3"
 
     from_str = staticmethod(make_str_to_enum(MAPPING))
