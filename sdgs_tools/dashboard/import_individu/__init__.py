e9b7010e71173b0d7c273eeebc04cd050d068eef
hexatester <revolusi147id@gmail.com>
2021-09-05 18:17:51 +0700

    Add MappingIndividu

--- sdgs_tools/dashboard/import_individu/__init__.py
+++ sdgs_tools/dashboard/import_individu/__init__.py
@@ -4,12 +4,14 @@ from .penghasilan import Penghasilan
 from .penyakit_diderita import PenyakitDiderita
 
 from .data_individu import DataIndividu
+from .mapping import MappingIndividu
 
 
 __all__ = [
     "DataIndividu",
     "Disabilitas",
     "FasilitasKesehatan",
+    "MappingIndividu",
     "Penghasilan",
     "PenyakitDiderita",
 ]
