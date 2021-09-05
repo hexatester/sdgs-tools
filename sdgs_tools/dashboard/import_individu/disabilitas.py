6a32b799ed1c90723fa22cacf5b629f6bfb1b7dd
hexatester <revolusi147id@gmail.com>
2021-09-05 17:00:19 +0700

    Added Disabilitas.from_str

--- sdgs_tools/dashboard/import_individu/disabilitas.py
+++ sdgs_tools/dashboard/import_individu/disabilitas.py
@@ -5,18 +5,32 @@ from typing import Dict
 from sdgs_tools.dashboard.enums import YaTidak
 
 
+MAPPING = {
+    "Tunanetra": "tunanetra",
+    "Tunarungu": "tunarungu",
+    "Tunawicara": "tunawicara",
+    "Tunarungu_wicara": "tunarungu_wicara",
+    "Tunadaksa": "tunadaksa",
+    "Tunagrahita": "tunagrahita",
+    "Tunalaras": "tunalaras",
+    "Cacat Eks Kusta": "cacat_eks_kusta",
+    "Cacat Ganda": "cacat_ganda",
+    "Dipasung": "dipasung",
+}
+
+
 @attr.dataclass
 class Disabilitas:
-    tunanetra: YaTidak
-    tunarungu: YaTidak
-    tunawicara: YaTidak
-    tunarungu_wicara: YaTidak
-    tunadaksa: YaTidak
-    tunagrahita: YaTidak
-    tunalaras: YaTidak
-    cacat_eks_kusta: YaTidak
-    cacat_ganda: YaTidak
-    dipasung: YaTidak
+    tunanetra: YaTidak = YaTidak.TIDAK
+    tunarungu: YaTidak = YaTidak.TIDAK
+    tunawicara: YaTidak = YaTidak.TIDAK
+    tunarungu_wicara: YaTidak = YaTidak.TIDAK
+    tunadaksa: YaTidak = YaTidak.TIDAK
+    tunagrahita: YaTidak = YaTidak.TIDAK
+    tunalaras: YaTidak = YaTidak.TIDAK
+    cacat_eks_kusta: YaTidak = YaTidak.TIDAK
+    cacat_ganda: YaTidak = YaTidak.TIDAK
+    dipasung: YaTidak = YaTidak.TIDAK
 
     def todict(self) -> Dict[str, str]:
         return {
@@ -40,6 +54,18 @@ class Disabilitas:
             "18": "2",
             "19": "2",
         }
+
+    @classmethod
+    def from_str(cls, val: str):
+        if not val:
+            return cls()
+        data: Dict[str, YaTidak] = dict()
+        for key, name in MAPPING.items():
+            if key in val:
+                data[name] = YaTidak.YA
+            else:
+                data[name] = YaTidak.TIDAK
+        return cls(**data)
 
 
 cattr.register_unstructure_hook(Disabilitas, Disabilitas.todict)
