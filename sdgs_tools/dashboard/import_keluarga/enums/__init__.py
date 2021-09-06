d12bbafe1e056eb0c1d9478c580c5663a8268882
hexatester <revolusi147id@gmail.com>
2021-09-07 00:32:27 +0700

    Added Dinding

--- sdgs_tools/dashboard/import_keluarga/enums/__init__.py
+++ sdgs_tools/dashboard/import_keluarga/enums/__init__.py
@@ -1,9 +1,12 @@
 import cattr
 
 from .atap import Atap
+from .dinding import Dinding
 
 cattr.register_structure_hook(Atap, Atap.from_str)  # type: ignore
+cattr.register_structure_hook(Dinding, Dinding.from_str)  # type: ignore
 
 __all__ = [
     "Atap",
+    "Dinding",
 ]
---
+++ sdgs_tools/dashboard/import_keluarga/enums/dinding.py
@@ -0,0 +1,16 @@
+from enum import Enum
+from sdgs_tools.dashboard.utils import make_str_to_enum
+
+MAPPING = {
+    "Semen/beton/kayu berkualitas tinggi": "1",
+    "Kayu berkualitas rendah/bamboo": "2",
+    "Lainnya": "other",
+}
+
+
+class Dinding(Enum):
+    SEMEN_BETON_KAYU_KT = "1"
+    KAYU_BAMBU_KR = "2"
+    LAINNYA = "other"
+
+    from_str = classmethod(make_str_to_enum(MAPPING, "other"))
