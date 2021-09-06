e418bce459473e4a63836091418682264e9cbbfb
hexatester <revolusi147id@gmail.com>
2021-09-07 00:34:41 +0700

    Added FasilitasBab

--- sdgs_tools/dashboard/import_keluarga/enums/__init__.py
+++ sdgs_tools/dashboard/import_keluarga/enums/__init__.py
@@ -4,15 +4,18 @@ from .atap import Atap
 from .dinding import Dinding
 from .energi_memasak import EnergiMemasak
 from .fasilitas_bab import FasilitasMck
+from .fasilitas_mck import FasilitasBab
 
 cattr.register_structure_hook(Atap, Atap.from_str)  # type: ignore
 cattr.register_structure_hook(Dinding, Dinding.from_str)  # type: ignore
 cattr.register_structure_hook(EnergiMemasak, EnergiMemasak.from_str)  # type: ignore
 cattr.register_structure_hook(FasilitasMck, FasilitasMck.from_str)  # type: ignore
+cattr.register_structure_hook(FasilitasBab, FasilitasBab.from_str)  # type: ignore
 
 __all__ = [
     "Atap",
     "Dinding",
     "EnergiMemasak",
     "FasilitasMck",
+    "FasilitasBab",
 ]
---
+++ sdgs_tools/dashboard/import_keluarga/enums/fasilitas_mck.py
@@ -0,0 +1,18 @@
+from enum import Enum
+from sdgs_tools.dashboard.utils import make_str_to_enum
+
+MAPPING = {
+    "Sendiri": "1",
+    "Berkelompok/tetangga": "2",
+    "MCK umum": "3",
+    "Tidak ada": "4",
+}
+
+
+class FasilitasBab(Enum):
+    SENDIRI = "1"
+    BERKELOMPOK = "2"
+    UMUM = "3"
+    TIDAK_ADA = "4"
+
+    from_str = classmethod(make_str_to_enum(MAPPING, "1"))
