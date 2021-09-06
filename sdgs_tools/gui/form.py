fdca2645b896fae60c1f2c287871f58077a345f3
hexatester <revolusi147id@gmail.com>
2021-09-06 11:35:51 +0700

    Initial TextForm

---
+++ sdgs_tools/gui/form.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+
+class TextForm:
+    def __init__(
+        self,
+        master,
+        text: str,
+        row: int,
+        value: str = None,
+        name: str = None,
+        start_col: int = 0,
+    ):
+        super().__init__()
+        self.value = tk.StringVar(master, value, name)
+        self.label = tk.Label(master, text=text)
+        self.label.pack(row=row, column=start_col)
+        self.entry = tk.Entry(master, textvariable=self.value)
+        self.entry.pack(row=row + 1, column=start_col + 1)
+
+    def __str__(self):
+        return self.value.get()
