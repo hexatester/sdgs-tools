d8449524ebf9c1a5fb2498a2b6be722f1e5ae60c
hexatester <revolusi147id@gmail.com>
2021-09-06 11:53:30 +0700

    Update BaseAuth

--- sdgs_tools/dashboard/auth/base.py
+++ sdgs_tools/dashboard/auth/base.py
@@ -1,3 +1,4 @@
+import click
 from ..response import SdgsResponse
 from ..base import BaseSdgs
 
@@ -8,23 +9,29 @@ class BaseAuth(BaseSdgs):
     def login(self, username: str, password: str) -> AuthToken:
         res = self.session.get("https://dashboard-sdgs.kemendesa.go.id/")
         # AuthToken
-        assert res.ok
+        if not res.ok:
+            raise Exception("Server tidak merespon, mohon coba beberapa saat lagi")
         url = self.url_api("auth/token")
         data = {"username": username, "password": password}
         res = self.session.post(url, data)
-        assert res.ok
+        if not res.ok:
+            raise Exception("Server tidak merespon, mohon coba beberapa saat lagi")
         res_data = SdgsResponse.from_str(res.text, AuthToken)
+        if res_data.status == 500:
+            raise Exception(res_data.message)
         self.session.headers.update(res_data.data.token.token_header)
         return res_data.data
 
     def token_refresh(self, token: AuthToken) -> AuthToken:
+        click.echo("Mencoba refresh token")
         url = self.url_api("auth/token/refresh")
         data = {
             "refreshToken": token.token.refresh_token,
             "username": token.user.username,
         }
         res = self.session.post(url, data)
-        assert res.ok
+        if not res.ok:
+            raise Exception("Server tidak merespon")
         res_data = SdgsResponse.from_str(res.text, AuthToken)
         self.session.headers.update(res_data.data.token.token_header)
         return res_data.data
