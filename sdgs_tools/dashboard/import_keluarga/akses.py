bee6a3bd3bda9a1742cc7e9a3ffd11941f6f2a28
hexatester <revolusi147id@gmail.com>
2021-09-07 16:44:37 +0700

    Update Akses

--- sdgs_tools/dashboard/import_keluarga/akses.py
+++ sdgs_tools/dashboard/import_keluarga/akses.py
@@ -10,10 +10,14 @@ class Akses:
     waktu: str = "0"
     kemudahan: YaTidak = YaTidak.YA
 
+    def __attrs_post_init__(self):
+        waktu = int(self.waktu) / 60
+        self.waktu = str(waktu)
+
     @staticmethod
     def from_cols(ws: Worksheet, row: int, j: str, w: str, k: str):
         return {
-            "jarak": ws[f"{j}{row}"],
-            "waktu": ws[f"{w}{row}"],
-            "kemudahan": ws[f"{k}{row}"],
+            "jarak": ws[f"{j}{row}"].value,
+            "waktu": ws[f"{w}{row}"].value,
+            "kemudahan": ws[f"{k}{row}"].value,
         }
