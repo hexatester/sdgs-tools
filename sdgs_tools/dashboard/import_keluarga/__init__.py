8b434e9ee55b486554108440a5a4fb31b44d469f
hexatester <revolusi147id@gmail.com>
2021-09-07 17:24:10 +0700

    Added cattr hooks

--- sdgs_tools/dashboard/import_keluarga/__init__.py
+++ sdgs_tools/dashboard/import_keluarga/__init__.py
@@ -1,3 +1,5 @@
+import cattr
+
 from .akses import Akses
 from .sarpras_transport import SarprasTransport
 
@@ -7,6 +9,15 @@ from .akses_sarpras_transport import AksesSarprasTransport
 from .akses_tenaga_kesehatan import AksesTenagaKesehatan
 from .bantuam_pemerintah import BantuanPemerintah
 from .luas import Luas
+
+cattr.register_unstructure_hook(AksesFasilitasKesehatan, AksesFasilitasKesehatan.todict)
+cattr.register_unstructure_hook(AksesPendidikan, AksesPendidikan.todict)
+cattr.register_unstructure_hook(AksesSarprasTransport, AksesSarprasTransport.todict)
+cattr.register_unstructure_hook(AksesTenagaKesehatan, AksesTenagaKesehatan.todict)
+cattr.register_unstructure_hook(BantuanPemerintah, BantuanPemerintah.todict)
+cattr.register_unstructure_hook(AksesTenagaKesehatan, AksesTenagaKesehatan.todict)
+
+cattr.register_structure_hook(BantuanPemerintah, BantuanPemerintah.from_str)
 
 __all__ = [
     "AksesFasilitasKesehatan",
