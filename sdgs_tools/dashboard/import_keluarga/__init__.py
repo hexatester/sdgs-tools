812da6fcc4d9868235b9ed2afd949f94dcce9407
hexatester <revolusi147id@gmail.com>
2021-09-07 17:06:30 +0700

    Added AksesSarprasTransport

--- sdgs_tools/dashboard/import_keluarga/__init__.py
+++ sdgs_tools/dashboard/import_keluarga/__init__.py
@@ -3,12 +3,14 @@ from .sarpras_transport import SarprasTransport
 
 from .akses_fasilitas_kesehatan import AksesFasilitasPekerjaan
 from .akses_pendidikan import AksesPendidikan
+from .akses_sarpras_transport import AksesSarprasTransport
 from .akses_tenaga_kesehatan import AksesTenagaPekerjaan
 from .luas import Luas
 
 __all__ = [
     "AksesFasilitasPekerjaan",
     "AksesPendidikan",
+    "AksesSarprasTransport",
     "AksesTenagaPekerjaan",
     "Akses",
     "Luas",
---
+++ sdgs_tools/dashboard/import_keluarga/akses_sarpras_transport.py
@@ -0,0 +1,47 @@
+import attr
+import cattr
+from openpyxl.worksheet.worksheet import Worksheet
+from typing import Dict
+
+from . import SarprasTransport
+
+MAPPING = {
+    "K.P424_1pekerjaan": "pekerjaan",
+    "K.P424_2pertanian": "pertanian",
+    "K.P424_3sekolah": "sekolah",
+    "K.P424_4berobat": "berobat",
+    "K.P424_5ibadah": "ibadah",
+    "K.P424_6rekreasi": "rekreasi",
+}
+
+MAPPING_VALUE = {
+    "pekerjaan": ("DE", "DF", "DG", "DH", "DI"),
+    "pertanian": ("DJ", "DK", "DL", "DM", "DN"),
+    "sekolah": ("DO", "DP", "DQ", "DR", "DS"),
+    "berobat": ("DT", "DU", "DV", "DW", "DX"),
+    "ibadah": ("DY", "DZ", "EA", "EB", "EC"),
+    "rekreasi": ("ED", "EE", "EF", "EG", "EH"),
+}
+
+
+@attr.dataclass
+class AksesSarprasTransport:
+    spesialis: SarprasTransport
+    dokter: SarprasTransport
+    bidan: SarprasTransport
+    nakes: SarprasTransport
+    dukun: SarprasTransport
+
+    def todict(self) -> Dict[str, Dict[str, str]]:
+        data: Dict[str, Dict[str, str]] = dict()
+        for key, name in MAPPING.items():
+            value: SarprasTransport = getattr(self, name)
+            data[key] = cattr.unstructure(value)
+        return data
+
+    @staticmethod
+    def make(ws: Worksheet, row: int) -> Dict[str, Dict[str, str]]:
+        data: Dict[str, Dict[str, str]] = dict()
+        for name, args in MAPPING_VALUE.items():
+            data[name] = SarprasTransport.from_cols(ws, row, *args)
+        return data
