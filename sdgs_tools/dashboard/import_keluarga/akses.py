77c263bd94284330f82aa52d43c1602b3849d41d
hexatester <revolusi147id@gmail.com>
2021-09-07 16:31:09 +0700

    Update Akses

--- sdgs_tools/dashboard/import_keluarga/akses.py
+++ sdgs_tools/dashboard/import_keluarga/akses.py
@@ -1,12 +1,14 @@
 import attr
 from openpyxl.worksheet.worksheet import Worksheet
 
+from .enums import YaTidak
+
 
 @attr.dataclass
 class Akses:
-    jarak: str
-    waktu: str
-    kemudahan: str
+    jarak: str = "0"
+    waktu: str = "0"
+    kemudahan: YaTidak = YaTidak.YA
 
     @staticmethod
     def from_cols(ws: Worksheet, row: int, j: str, w: str, k: str):
