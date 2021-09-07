ebb420e3a1e25fa2ca0214bdc422052d56a3310a
hexatester <revolusi147id@gmail.com>
2021-09-07 18:52:01 +0700

    Added ImportKeluargaWindow

--- sdgs_tools/gui/dashboard/__init__.py
+++ sdgs_tools/gui/dashboard/__init__.py
@@ -5,10 +5,12 @@ from tkinter.filedialog import asksaveasfile
 from tkinter.messagebox import showinfo, showwarning
 
 from .import_individu import ImportIndividuWindow
+from .import_keluarga import ImportKeluargaWindow
 
 
 class DashboardTab(tk.Frame):
     TEMPLATE_INDIVIDU = "https://hexatester.github.io/sdgs-tools/Template%20Individu%20Dashboard%20SDGS%20Kemendesa.xlsm"
+    TEMPLATE_KELUARGA = "https://hexatester.github.io/sdgs-tools/Template%20Keluarga%20Dashboard%20SDGS%20Kemendesa.xlsm"
 
     def __init__(self, master: tk.Frame, **kw):
         super().__init__(master, bg="#CCC", width=500, height=500, **kw)
@@ -19,6 +21,7 @@ class DashboardTab(tk.Frame):
             text="Fitur ini untuk meng import data ke dashboard-sdgs (MASIH DALAM PENGERJAAN)",
         )
         self.label_info.grid(row=0, column=0, columnspan=2)
+        # Individu
         self.template_individu_button = tk.Button(
             self,
             text="Buat Template Individu",
@@ -31,6 +34,19 @@ class DashboardTab(tk.Frame):
             command=ImportIndividuWindow,
         )
         self.import_individu_button.grid(row=1, column=1)
+        # Keluarga
+        self.template_keluarga_button = tk.Button(
+            self,
+            text="Buat Template Keluarga",
+            command=self.download_template_keluarga,
+        )
+        self.template_keluarga_button.grid(row=2, column=0)
+        self.import_keluarga_button = tk.Button(
+            self,
+            text="Import Keluarga",
+            command=ImportKeluargaWindow,
+        )
+        self.import_keluarga_button.grid(row=2, column=1)
 
     def download_template_individu(self):
         files = [
@@ -43,6 +59,21 @@ class DashboardTab(tk.Frame):
                 "Mohon tentukan nama dan lokasi file yang akan disimpan",
             )
         res = requests.get(self.TEMPLATE_INDIVIDU)
+        with open(filepath.name, "wb") as f:
+            f.write(res.content)
+        showinfo("Sukses", f"Berhasil menyimpan template di {filepath.name}")
+
+    def download_template_keluarga(self):
+        files = [
+            ("Excel 2010+", "*.xlsx"),
+        ]
+        filepath = asksaveasfile(filetypes=files, defaultextension=files)
+        if not filepath or not filepath.name:
+            showwarning(
+                "Gagal",
+                "Mohon tentukan nama dan lokasi file yang akan disimpan",
+            )
+        res = requests.get(self.TEMPLATE_KELUARGA)
         with open(filepath.name, "wb") as f:
             f.write(res.content)
         showinfo("Sukses", f"Berhasil menyimpan template di {filepath.name}")
---
+++ sdgs_tools/gui/dashboard/import_keluarga.py
@@ -0,0 +1,106 @@
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+from tkinter.messagebox import showerror, showinfo, showwarning
+from typing import Optional
+
+
+from sdgs_tools.utils import parse_range
+from sdgs_tools.dashboard.sdgs import Sdgs
+from sdgs_tools.dashboard.import_keluarga import import_keluarga
+from ..form import TextForm
+
+
+class ImportKeluargaWindow(tk.Toplevel):
+    def __init__(self, master=None):
+        super().__init__(master=master)
+        self.geometry("230x250")
+        self.sdgs: Optional[Sdgs] = None
+        self.user_label: Optional[tk.Label] = None
+        self.title("Import Keluarga")
+        self.label_info = tk.Label(
+            self,
+            text="Import data keluarga",
+        )
+        self.label_info.grid(row=0, column=0, columnspan=3)
+        # Autentikasi
+        self.username_form = TextForm(self, "Username", 1)
+        self.password_form = TextForm(self, "Password", 2)
+        self.login_button = tk.Button(
+            self,
+            text="Login",
+            command=self.login,
+        )
+        self.login_button.grid(row=3, column=2)
+
+    def show_import(self):
+        # Import?
+        self.baris_form = TextForm(self, "Baris", 5)
+        self.baris_info_button = tk.Button(self, text="?", command=self.info_baris)
+        self.baris_info_button.grid(row=5, column=2)
+        self.rt_form = TextForm(self, "Rt", 6)
+        self.rw_form = TextForm(self, "Rw", 7)
+        self.info_label = tk.Label(
+            self,
+            text="Unduh template di .....",
+        )
+        self.info_label.grid(row=9, column=0, columnspan=3)
+        self.start_button = tk.Button(
+            self,
+            text="Pilih template keluarga dan mulai import",
+            command=self.start_import,
+        )
+        self.start_button.grid(row=10, column=0, columnspan=3)
+
+    def login(self):
+        username: str = self.username_form.value.get()
+        password: str = self.password_form.value.get()
+        try:
+            self.sdgs = Sdgs(username, password)
+            showinfo(
+                "Sukses",
+                f"Berhasil login dengan menggunakan akun {self.sdgs.token.user}",
+            )
+            self.user_label = tk.Label(self, text=self.sdgs.token.user)
+            self.user_label.grid(row=3, column=0, columnspan=2)
+            self.show_import()
+        except Exception as e:
+            showerror("Gagal", f"Gagal login, alasan: {e}")
+
+    def start_import(self):
+        rt = self.rt_form.value.get()
+        rw = self.rw_form.value.get()
+        if len(rt) != 3:
+            showerror("Gagal", "Rt harus 3 digit, misal 001")
+            return
+        if len(rw) != 3:
+            showerror("Gagal", "Rw harus 3 digit, misal 001")
+            return
+        files = [
+            ("Excel Macro", "*.xlsm"),
+        ]
+        filepath = askopenfilename(filetypes=files, defaultextension=files)
+        if not filepath:
+            showwarning("Gagal", "Mohon pilih template yang benar")
+            return
+        try:
+            import_keluarga(
+                sdgs=self.sdgs,
+                filepath=filepath,
+                rows=parse_range(self.baris_form.value.get()),
+                rt=rt,
+                rw=rw,
+            )
+        except FileNotFoundError:
+            showwarning(
+                "Gagal",
+                "Gagal mengeksport data karena file template tidak ditemukan",
+            )
+        except Exception as e:
+            showerror("Error", f"Terjadi error : {e}")
+
+    @staticmethod
+    def info_baris():
+        showinfo(
+            "Baris?",
+            'Baris data dari template yang diambil datanya\nMisalnya "2-100,150-200"',
+        )
