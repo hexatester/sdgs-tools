7489d89106e6eaf55344de5458950381392cf072
hexatester <revolusi147id@gmail.com>
2021-09-07 18:33:47 +0700

    Added .make_json

--- sdgs_tools/dashboard/import_keluarga/data_keluarga.py
+++ sdgs_tools/dashboard/import_keluarga/data_keluarga.py
@@ -1,4 +1,5 @@
 import attr
+import cattr
 from openpyxl.worksheet.worksheet import Worksheet
 from typing import Any, Dict, Optional, Tuple
 
@@ -8,6 +9,7 @@ from . import (
     AksesPendidikan,
     AksesSarprasTransport,
     AksesTenagaKesehatan,
+    BantuanPemerintah,
 )
 from .enums import (
     Atap,
@@ -27,7 +29,7 @@ from .enums import (
     YaTidak,
 )
 
-MAPPING = {
+MAPPING_COLS = {
     "no_kk": "A",
     "nik": "B",
     "nama": "C",
@@ -39,7 +41,7 @@ MAPPING = {
     "tempat_tinggal_comment": "I",
     "status_lahan": "J",
     "status_lahan_comment": "K",
-    "luas": "LM",
+    # "luas": "LM",
     "lantai": "N",
     "lantai_other": "O",
     "dinding": "P",
@@ -53,6 +55,7 @@ MAPPING = {
     "pembuangan_sampah": "X",
     "fasilitas_mck": "Y",
     "sumber_air_mandi": "Z",
+    "sumber_air_mandi_comment": "AA",
     "fasilitas_bab": "AB",
     "fasilitas_bab_comment": "AC",
     "pembuangan_limbah_cair": "AE",
@@ -69,6 +72,57 @@ MAPPING = {
     "transport_umum_bulan_sebelumnya": "EJ",
     "penerima_program_pemerintah": "EK",
     "pengeluaran_bulanan": "EL",
+}
+
+MAPPING = {
+    "provinsi": "provinsi",
+    "kota": "kota",
+    "kecamatan": "kecamatan",
+    "desa": "desa",
+    "rt": "rt",
+    "rw": "rw",
+    "no_kk": "no_kk",
+    "nik": "nik",
+    "K.P206": "nama",
+    "K.P207": "alamat",
+    "K.P208": "nomor_hp",
+    "K.P209": "telepon_rumah",
+    "K.P303": "anggota_keluarga",
+    "K.P401": "tempat_tinggal",
+    "K.P401-Comment": "tempat_tinggal_comment",
+    "K.P402": "status_lahan",
+    "K.P402-Comment": "status_lahan_comment",
+    "K.P403": "luas",
+    "K.P404": "lantai",
+    "K.P404-Comment": "lantai_other",
+    "K.P405": "dinding",
+    "K.P405-Comment": "dinding_other",
+    "K.P407": "jendela",
+    "K.P406": "atap",
+    "K.P407-Comment": "atap_comment",
+    "K.P408": "penerangan",
+    "K.P409": "energi_memasak",
+    "K.P410": "sumber_kayu_bakar",
+    "K.P411": "pembuangan_sampah",
+    "K.P412": "fasilitas_mck",
+    "K.P413": "sumber_air_mandi",
+    "K.P413-Comment": "sumber_air_mandi_comment",
+    "K.P414": "fasilitas_bab",
+    "K.P414-Comment": "fasilitas_bab_comment",
+    "K.P416": "pembuangan_limbah_cair",
+    "K.P416-Comment": "pembuangan_limbah_cair_comment",
+    "K.P417": "bawah_sutet",
+    "K.P418": "bantaran_sungai",
+    "K.P419": "lereng_gunung",
+    "K.P420": "kumuh",
+    # "K.P421": "akses_pendidikan",
+    # "K.P422": "akses_fasilitas_kesehatan",
+    # "K.P423": "akses_tenaga_kesehatan",
+    # "K.P424": "akses_sarpras_transport",
+    "K.P425": "transport_umum",
+    "K.P426": "transport_umum_bulan_sebelumnya",
+    "K.P427": "penerima_program_pemerintah",
+    "K.P428": "pengeluaran_bulanan",
 }
 
 
@@ -100,6 +154,7 @@ class DataKeluarga:
     pembuangan_sampah: PembuanganSampah
     fasilitas_mck: FasilitasMck
     sumber_air_mandi: SumberAirMandi
+    sumber_air_mandi_comment: Optional[str]
     fasilitas_bab: FasilitasBab
     fasilitas_bab_comment: Optional[str] = None
     pembuangan_limbah_cair: PembuanganLimbahCair
@@ -119,14 +174,15 @@ class DataKeluarga:
     # LAIN-LAIN
     transport_umum: str
     transport_umum_bulan_sebelumnya: str
-    penerima_program_pemerintah: str
+    penerima_program_pemerintah: BantuanPemerintah
     pengeluaran_bulanan: str
 
     @staticmethod
     def make_data(ws: Worksheet, row: int):
         data: Dict[str, Any] = dict()
-        for name, col in MAPPING.items():
+        for name, col in MAPPING_COLS.items():
             data[name] = ws[f"{col}{row}"].value
+        data["luas"] = Luas.make(ws, row)
         data["akses_pendidikan"] = AksesPendidikan.make(ws, row)
         data["akses_fasilitas_kesehatan"] = AksesFasilitasKesehatan.make(ws, row)
         data["akses_tenaga_kesehatan"] = AksesTenagaKesehatan.make(ws, row)
@@ -136,6 +192,31 @@ class DataKeluarga:
     @staticmethod
     def get_kk_nik(ws: Worksheet, row: int) -> Tuple[str, str]:
         return (
-            ws[f"{MAPPING['no_kk']}{row}"].value,
-            ws[f"{MAPPING['nik']}{row}"].value,
+            ws[f"{MAPPING_COLS['no_kk']}{row}"].value,
+            ws[f"{MAPPING_COLS['nik']}{row}"].value,
         )
+
+    def make_json(self, desa: str, rt: str, rw: str):
+        raw_data: Dict[str, Any] = cattr.unstructure(self)
+        clean_data: Dict[str, Any] = dict()
+        for key, properti in MAPPING.items():
+            value = raw_data.get(properti)
+            if value in (None, "None"):
+                # TODO Improve empty value detection
+                continue
+            clean_data[key] = value
+        clean_data.update(self.akses_pendidikan.todict())
+        clean_data.update(self.akses_tenaga_kesehatan.todict())
+        clean_data.update(self.akses_tenaga_kesehatan.todict())
+        clean_data.update(self.akses_sarpras_transport.todict())
+        clean_data.update(
+            {
+                "desa": desa,
+                "kecamatan": desa[0:7],
+                "kota": desa[0:4],
+                "provinsi": desa[0:2],
+                "rt": rt,
+                "rw": rw,
+            }
+        )
+        return clean_data
