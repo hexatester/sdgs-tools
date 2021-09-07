1ed45f405119d2433245762550c1a48494546bbd
hexatester <revolusi147id@gmail.com>
2021-09-07 18:32:39 +0700

    Fix typo

--- sdgs_tools/dashboard/import_keluarga/__init__.py
+++ sdgs_tools/dashboard/import_keluarga/__init__.py
@@ -13,7 +13,7 @@ from .akses_fasilitas_kesehatan import AksesFasilitasKesehatan
 from .akses_pendidikan import AksesPendidikan
 from .akses_sarpras_transport import AksesSarprasTransport
 from .akses_tenaga_kesehatan import AksesTenagaKesehatan
-from .bantuam_pemerintah import BantuanPemerintah
+from .bantuan_pemerintah import BantuanPemerintah
 from .luas import Luas
 
 from .data_keluarga import DataKeluarga
