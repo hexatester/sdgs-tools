a88a8def270bb545ef83e823e968663c3fac22f8
hexatester <revolusi147id@gmail.com>
2021-09-07 18:31:43 +0700

    Initial Luas.make

--- sdgs_tools/dashboard/import_keluarga/luas.py
+++ sdgs_tools/dashboard/import_keluarga/luas.py
@@ -1,7 +1,12 @@
 import attr
+from openpyxl.worksheet.worksheet import Worksheet
 
 
 @attr.dataclass
 class Luas:
     lantai: str
     lahan: str
+
+    @staticmethod
+    def make(ws: Worksheet, row: int):
+        return {"lantai": ws[f"L{row}"].value, "lahan": ws[f"M{row}"].value}
