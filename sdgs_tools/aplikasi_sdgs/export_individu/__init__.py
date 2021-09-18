de6c2de41f676a933299e39be9f6225b0f983f1e
hexatester <revolusi147id@gmail.com>
2021-09-18 14:38:29 +0700

    Initial get_data_individu

---
+++ sdgs_tools/aplikasi_sdgs/export_individu/__init__.py
@@ -0,0 +1,6 @@
+from .individu import get_data_individu
+
+
+__all__ = [
+    "get_data_individu",
+]
---
+++ sdgs_tools/aplikasi_sdgs/export_individu/individu.py
@@ -0,0 +1,43 @@
+from typing import Any, Dict
+from uiautomator2 import Device, UiObjectNotFoundError
+
+from sdgs_tools.aplikasi_sdgs.utils import d_get_text, menu_to
+
+INDIVIDU_COL = {
+    "nama": "com.kemendes.survey:id/txtNama",
+    "jenis_kelamin": "com.kemendes.survey:id/cbJenisKelamin",
+    "tempat_lahir": "com.kemendes.survey:id/txtTempatLahir",
+    "tanggal_lahir": "com.kemendes.survey:id/txtTglLahir",
+    "usia": "com.kemendes.survey:id/txtUsia",
+    "status_pernikahan": "com.kemendes.survey:id/cbStatus",
+    "usia_menikah": "com.kemendes.survey:id/txtUsiaSaatNikah",
+    "agama": "com.kemendes.survey:id/cbAgama",
+    "suku_bangsa": "com.kemendes.survey:id/txtSuku",
+    "warga_negara": "com.kemendes.survey:id/cbWargaNegara",
+    "nomor_hp": "com.kemendes.survey:id/txtNoHP",
+    "nomor_whatsapp": "com.kemendes.survey:id/txtNoWA",
+    "alamat_email": "com.kemendes.survey:id/txtEmail",
+    "alamat_facebook": "com.kemendes.survey:id/txtFB",
+    "alamat_twitter": "com.kemendes.survey:id/txtTwitter",
+    "alamat_instagram": "com.kemendes.survey:id/txtInstagram",
+    "aktif_internet": "com.kemendes.survey:id/cbInternet",
+    # "akses_melalui": "com.kemendes.survey:id/cbAksesInternetLewat",
+    # "kecepatan_internet": "com.kemendes.survey:id/cbKecepatanInternet",
+}
+
+
+def get_data_individu(d: Device) -> Dict[str, Any]:
+    menu_to(d, "DATA INDIVIDU")
+    data: Dict[str, Any] = dict()
+    # d(text="DATA INDIVIDU").click()
+    for name, resourceId in INDIVIDU_COL.items():
+        try:
+            data[name] = d_get_text(d, resourceId)
+        except UiObjectNotFoundError:
+            continue
+    if data["aktif_internet"] == "Ya":
+        # Internet = Ya
+        data["akses_melalui"] = d_get_text(d, "com.kemendes.survey:id/cbAksesInternetLewat")
+        data["kecepatan_internet"] = d_get_text(d, "com.kemendes.survey:id/cbKecepatanInternet")
+    d(className="android.widget.ScrollView").fling.vert.backward()
+    return data
