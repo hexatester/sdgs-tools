d2f204ac400de39ce7c3a07fded74972f67a64b8
hexatester <revolusi147id@gmail.com>
2021-09-20 23:54:37 +0700

    Refactor ExportIndividuWindow

--- sdgs_tools/gui/aplikasi/export_individu.py
+++ sdgs_tools/gui/aplikasi/export_individu.py
@@ -3,8 +3,10 @@ from tkinter.filedialog import askopenfilename
 from tkinter.messagebox import showerror, showinfo, showwarning
 from uiautomator2 import connect
 
-from sdgs_tools.utils import parse_range
+from sdgs_tools.gui.form import TextForm
+from sdgs_tools.gui.ya_tidak import YaTidakForm
 from sdgs_tools.aplikasi_sdgs.export_individu import export_individu as _export_individu
+from sdgs_tools.utils import parse_range
 
 
 class ExportIndividuWindow(tk.Toplevel):
@@ -17,101 +19,39 @@ class ExportIndividuWindow(tk.Toplevel):
         )
         self.label_info.grid(row=0, column=0, columnspan=3)
         # Baris
-        self.baris_label = tk.Label(self, text="Baris")
-        self.baris_label.grid(row=1, column=0)
-        self.baris = tk.StringVar(self, "1")
-        self.baris_entry = tk.Entry(self, textvariable=self.baris)
-        self.baris_entry.grid(row=1, column=1)
+        self.baris = TextForm(self, "Baris", 1)
         self.baris_info_button = tk.Button(self, text="?", command=self.info_baris)
         self.baris_info_button.grid(row=1, column=2)
+        # RT/RW
+        self.rt_rw = TextForm(self, "RT/RW", 2)
         # Individu
-        self.individu_label = tk.Label(self, text="Ekspor data individu")
-        self.individu_label.grid(row=2, column=0)
-        self.individu = tk.BooleanVar(self, value=True)
-        self.individu_ya = tk.Radiobutton(
-            self, text="Ya", variable=self.individu, value=True
-        )
-        self.individu_ya.grid(row=2, column=1)
-        self.individu_tidak = tk.Radiobutton(
-            self, text="Tidak", variable=self.individu, value=False
-        )
-        self.individu_tidak.grid(row=2, column=2)
+        self.individu = YaTidakForm(self, "Ekspor data individu", 3, True)
         # Pekerjaan
-        self.pekerjaan_label = tk.Label(self, text="Ekspor data pekerjaan")
-        self.pekerjaan_label.grid(row=3, column=0)
-        self.pekerjaan = tk.BooleanVar(self, value=True)
-        self.pekerjaan_ya = tk.Radiobutton(
-            self, text="Ya", variable=self.pekerjaan, value=True
-        )
-        self.pekerjaan_ya.grid(row=3, column=1)
-        self.pekerjaan_tidak = tk.Radiobutton(
-            self, text="Tidak", variable=self.pekerjaan, value=False
-        )
-        self.pekerjaan_tidak.grid(row=3, column=2)
+        self.pekerjaan = YaTidakForm(self, "Ekspor data pekerjaan", 4, True)
         # Penghasilan
-        self.penghasilan_label = tk.Label(self, text="Ekspor data penghasilan")
-        self.penghasilan_label.grid(row=4, column=0)
-        self.penghasilan = tk.BooleanVar(self, value=True)
-        self.penghasilan_ya = tk.Radiobutton(
-            self, text="Ya", variable=self.penghasilan, value=True
-        )
-        self.penghasilan_ya.grid(row=4, column=1)
-        self.penghasilan_tidak = tk.Radiobutton(
-            self, text="Tidak", variable=self.penghasilan, value=False
-        )
-        self.penghasilan_tidak.grid(row=4, column=2)
+        self.penghasilan = YaTidakForm(self, "Ekspor data penghasilan", 5, True)
         # Kesehatan
-        self.kesehatan_label = tk.Label(self, text="Ekspor data kesehatan")
-        self.kesehatan_label.grid(row=5, column=0)
-        self.kesehatan = tk.BooleanVar(self, value=True)
-        self.kesehatan_ya = tk.Radiobutton(
-            self, text="Ya", variable=self.kesehatan, value=True
-        )
-        self.kesehatan_ya.grid(row=5, column=1)
-        self.kesehatan_tidak = tk.Radiobutton(
-            self, text="Tidak", variable=self.kesehatan, value=False
-        )
-        self.kesehatan_tidak.grid(row=5, column=2)
+        self.kesehatan = YaTidakForm(self, "Ekspor data kesehatan", 6, True)
         # Disabilitas
-        self.disabilitas_label = tk.Label(self, text="Ekspor data disabilitas")
-        self.disabilitas_label.grid(row=5, column=0)
-        self.disabilitas = tk.BooleanVar(self, value=True)
-        self.disabilitas_ya = tk.Radiobutton(
-            self, text="Ya", variable=self.disabilitas, value=True
-        )
-        self.disabilitas_ya.grid(row=5, column=1)
-        self.disabilitas_tidak = tk.Radiobutton(
-            self, text="Tidak", variable=self.disabilitas, value=False
-        )
-        self.disabilitas_tidak.grid(row=5, column=2)
+        self.disabilitas = YaTidakForm(self, "Ekspor data disabilitas", 7, True)
         # Pendidikan
-        self.pendidikan_label = tk.Label(self, text="Ekspor data pendidikan")
-        self.pendidikan_label.grid(row=6, column=0)
-        self.pendidikan = tk.BooleanVar(self, value=True)
-        self.pendidikan_ya = tk.Radiobutton(
-            self, text="Ya", variable=self.pendidikan, value=True
-        )
-        self.pendidikan_ya.grid(row=6, column=1)
-        self.pendidikan_tidak = tk.Radiobutton(
-            self, text="Tidak", variable=self.pendidikan, value=False
-        )
-        self.pendidikan_tidak.grid(row=6, column=2)
+        self.pendidikan = YaTidakForm(self, "Ekspor data pendidikan", 8, True)
         self.info_label = tk.Label(
             self,
             text="Buat template dan isi dengan rt rw, no kk, "
             "dan nik dari data yang ingin diekspor dari aplikasi!",
         )
-        self.info_label.grid(row=7, column=0, columnspan=3)
+        self.info_label.grid(row=9, column=0, columnspan=3)
         self.start_button = tk.Button(
             self,
             text="Pilih template individu dan mulai ekspor",
             command=self.export,
         )
-        self.start_button.grid(row=8, column=0)
+        self.start_button.grid(row=10, column=0)
 
     def export(self):
         files = [
-            ("Excel 2010+", "*.xlsx"),
+            ("Excel + Macro", "*.xlsm"),
         ]
         filepath = askopenfilename(filetypes=files, defaultextension=files)
         if not filepath:
@@ -121,6 +61,7 @@ class ExportIndividuWindow(tk.Toplevel):
             _export_individu(
                 d=connect(),
                 filepath=filepath,
+                rt_rw=str(self.rt_rw),
                 rows=parse_range(self.baris.get()),
                 start_row_penghasilan=2,
                 skip_individu=not self.individu.get(),
