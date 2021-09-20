dd4fa81b131c0e29f2b683939c94eb8a0b491a02
hexatester <revolusi147id@gmail.com>
2021-09-20 22:40:26 +0700

    Replace wrong get_data_penghasilan

--- sdgs_tools/aplikasi_sdgs/export_individu/penghasilan.py
+++ sdgs_tools/aplikasi_sdgs/export_individu/penghasilan.py
@@ -3,6 +3,25 @@ from uiautomator2 import Device, UiObject
 
 from sdgs_tools.aplikasi_sdgs.utils import d_get_text, menu_to
 
+SUBTITUTE = {
+    "Palawija (jagung,kacang-kacangan,ubi-ubian,dll)": "Palawija",
+    "Hortikultura (buah-buahan,sayur-sayuran,tanaman hias,obat2an)": "Hortikultura",
+    "Ternak  besar lainnya (kuda, kerbau, dll)": "Ternak  besar lainnya",
+    "Ayam pedaging": "Ayam pedaging",
+    "Telur ayam": "Telur ayam",
+    "Ternak kecil lainnya (bebek, burung, dll)": "Ternak kecil lainnya",
+    "Perikanan tangkap (termasuk biota lainnya)": "Perikanan tangkap",
+    "Perikanan budidaya (termasuk biota lainnya)": "Perikanan budidaya",
+    "Budidaya tanaman kehutanan (jati, mahoni, sengon, dll)": "Budidaya tanaman kehutanan",
+    "Pemungutan hasil hutan (madu, gaharu, buah-buahan, kayu bakar, rotan, dll)": "Pemungutan hasil hutan",
+    "Penangkapan satwa liar (babi, ayam hutan, kijang, dll)": "Penangkapan satwa liar",
+    "Penangkaran satwa liar (arwana, buaya, dll)": "Penangkaran satwa liar",
+    "Jasa pertanian (sewa traktor, penggilingan, dll)": "Jasa pertanian",
+    "Uang Pensiun": "Uang Pensiunan",
+    "TKI di luar negeri": "TKI di luar negeri",
+    "Sumbangan (dari keluarga, dari pemerintah)": "Sumbangan",
+}
+
 # PER BOX! resourceId
 PENGHASILAN_COL = {
     # "A": "NIK",
@@ -40,6 +59,9 @@ def get_data_penghasilan(d: Device) -> List[Dict[str, Any]]:
         data: Dict[str, Any] = dict()
         for name, resourceId_lstrip in PENGHASILAN_COL.items():
             data[name] = get_text(box_penghasilan, *resourceId_lstrip)
+        for old, new in SUBTITUTE.items():
+            if data["pekerjaan_utama"] == old:
+                data["pekerjaan_utama"] = new
         results.append(data)
     d(className="android.widget.ScrollView").fling.vert.backward()
     return results
