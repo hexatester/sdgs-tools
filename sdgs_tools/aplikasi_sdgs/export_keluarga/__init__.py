5c4fcbdd3ee382ad5abcae7a95463fa8fecfc2c8
hexatester <revolusi147id@gmail.com>
2021-09-20 15:53:06 +0700

    Added get_data_sarpras

--- sdgs_tools/aplikasi_sdgs/export_keluarga/__init__.py
+++ sdgs_tools/aplikasi_sdgs/export_keluarga/__init__.py
@@ -1,6 +1,7 @@
 from .kesehatan import get_data_kesehatan
 from .lokasi import get_data_lokasi
 from .pendidikan import get_data_pendidikan
+from .sarpras import get_data_sarpras
 from .tenaga_kesehatan import get_data_tenaga_kesehatan
 
 
@@ -8,5 +9,6 @@ __all__ = [
     "get_data_kesehatan",
     "get_data_lokasi",
     "get_data_pendidikan",
+    "get_data_sarpras",
     "get_data_tenaga_kesehatan",
 ]
---
+++ sdgs_tools/aplikasi_sdgs/export_keluarga/sarpras.py
@@ -0,0 +1,59 @@
+from typing import Any, Dict
+from uiautomator2 import Device, UiObject
+
+from sdgs_tools.aplikasi_sdgs.utils import d_get_text, menu_to, swipe_box
+
+AKSES_SARPRAS = {
+    "PEKERJAAN": "pekerjaan",
+    "PERTANIAN": "pertanian",
+    "SEKOLAH": "sekolah",
+    "BEROBAT": "berobat",
+    "IBADAH": "ibadah",
+    "REKREASI": "rekreasi",
+}
+
+SARPRAS_COL = {
+    # "B": ("com.kemendes.survey:id/txtNIK", ""),
+    # "C": ("com.kemendes.survey:id/txtNama", "Tujuan : Lokasi pekerjaan utama Jenis Transportasi : Darat"),
+    # "D": ("com.kemendes.survey:id/txtAlamat", "Waktu Tempuh :0.25 jam, Biaya : 5000"),
+    "kemudahan": ("com.kemendes.survey:id/txtTelpon", "Kemudahan :"),
+    # "H": ("com.kemendes.survey:id/txtStatus", ""),
+}
+
+
+def get_data_sarpras(d: Device) -> Dict[str, Dict[str, Any]]:
+    menu_to(d, "AKSES SARANA PRASARANA")
+    sarpras: Dict[str, Dict[str, Any]] = dict()
+    box_daftar_sarpras: UiObject = d(resourceId="com.kemendes.survey:id/itemsSarana")
+    survey_boxes: UiObject = box_daftar_sarpras.child(
+        resourceId="com.kemendes.survey:id/box"
+    )
+    for survey_box in survey_boxes:
+        data: Dict[str, Any] = dict()
+        # Kemudahan
+        kemudahan_box = survey_box.child(resourceId="com.kemendes.survey:id/txtTelpon")
+        kemudahan: str = kemudahan_box.info.get("text")
+        data["kemudahan"] = kemudahan.lstrip("Kemudahan :")
+        # Tujuan & Jenis
+        tujuan_jenis: str = survey_box.child(
+            resourceId="com.kemendes.survey:id/txtNama"
+        ).info.get("text")
+        tujuan_jenis = tujuan_jenis.lstrip("Tujuan : ")
+        tujuan_jenis = tujuan_jenis.replace(" Jenis Transportasi", "")
+        tujuan, jenis = tujuan_jenis.split(":")
+        nama = tujuan.strip()
+        data["jenis"] = jenis.strip()
+        # Waktu & Biaya
+        waktu_biaya: str = survey_box.child(
+            resourceId="com.kemendes.survey:id/txtAlamat"
+        ).info.get("text")
+        waktu_biaya = waktu_biaya.lstrip("Waktu Tempuh :")
+        waktu_biaya = waktu_biaya.replace(", Biaya", "")
+        waktu, biaya = waktu_biaya.split(":")
+        data["waktu"] = waktu.strip()
+        data["biaya"] = biaya.strip()
+        swipe_box(d, survey_box)
+        # TODO Find The Key
+        sarpras[AKSES_SARPRAS[nama]] = data
+    d(className="android.widget.ScrollView").fling.vert.backward()
+    return sarpras
