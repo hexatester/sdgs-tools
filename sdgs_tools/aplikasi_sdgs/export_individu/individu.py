e801087663996949216dd2e3a07af768d022518d
hexatester <revolusi147id@gmail.com>
2021-09-21 21:13:10 +0700

    Added default

--- sdgs_tools/aplikasi_sdgs/export_individu/individu.py
+++ sdgs_tools/aplikasi_sdgs/export_individu/individu.py
@@ -5,23 +5,23 @@ from uiautomator2 import Device, UiObjectNotFoundError
 from sdgs_tools.aplikasi_sdgs.utils import d_get_text, menu_to
 
 INDIVIDU_COL = {
-    "nama": "com.kemendes.survey:id/txtNama",
-    "jenis_kelamin": "com.kemendes.survey:id/cbJenisKelamin",
-    "tempat_lahir": "com.kemendes.survey:id/txtTempatLahir",
-    "tanggal_lahir": "com.kemendes.survey:id/txtTglLahir",
-    "usia": "com.kemendes.survey:id/txtUsia",
-    "status_pernikahan": "com.kemendes.survey:id/cbStatus",
-    "usia_menikah": "com.kemendes.survey:id/txtUsiaSaatNikah",
-    "agama": "com.kemendes.survey:id/cbAgama",
-    "suku_bangsa": "com.kemendes.survey:id/txtSuku",
-    "warga_negara": "com.kemendes.survey:id/cbWargaNegara",
-    "nomor_hp": "com.kemendes.survey:id/txtNoHP",
-    "nomor_whatsapp": "com.kemendes.survey:id/txtNoWA",
-    "alamat_email": "com.kemendes.survey:id/txtEmail",
-    "alamat_facebook": "com.kemendes.survey:id/txtFB",
-    "alamat_twitter": "com.kemendes.survey:id/txtTwitter",
-    "alamat_instagram": "com.kemendes.survey:id/txtInstagram",
-    "aktif_internet": "com.kemendes.survey:id/cbInternet",
+    "nama": ("com.kemendes.survey:id/txtNama", None),
+    "jenis_kelamin": ("com.kemendes.survey:id/cbJenisKelamin", None),
+    "tempat_lahir": ("com.kemendes.survey:id/txtTempatLahir", None),
+    "tanggal_lahir": ("com.kemendes.survey:id/txtTglLahir", None),
+    "usia": ("com.kemendes.survey:id/txtUsia", None),
+    "status_pernikahan": ("com.kemendes.survey:id/cbStatus", None),
+    "usia_menikah": ("com.kemendes.survey:id/txtUsiaSaatNikah", None),
+    "agama": ("com.kemendes.survey:id/cbAgama", None),
+    "suku_bangsa": ("com.kemendes.survey:id/txtSuku", None),
+    "warga_negara": ("com.kemendes.survey:id/cbWargaNegara", None),
+    "nomor_hp": ("com.kemendes.survey:id/txtNoHP", None),
+    "nomor_whatsapp": ("com.kemendes.survey:id/txtNoWA", "Nomor Untuk Whatsapp"),
+    "alamat_email": ("com.kemendes.survey:id/txtEmail", "Email"),
+    "alamat_facebook": ("com.kemendes.survey:id/txtFB", "Facebook"),
+    "alamat_twitter": ("com.kemendes.survey:id/txtTwitter", "Twitter"),
+    "alamat_instagram": ("com.kemendes.survey:id/txtInstagram", "Instagram"),
+    "aktif_internet": ("com.kemendes.survey:id/cbInternet", None),
     # "akses_melalui": "com.kemendes.survey:id/cbAksesInternetLewat",
     # "kecepatan_internet": "com.kemendes.survey:id/cbKecepatanInternet",
 }
@@ -31,8 +31,11 @@ def get_data_individu(d: Device) -> Dict[str, Any]:
     menu_to(d, "DATA INDIVIDU")
     data: Dict[str, Any] = dict()
     # d(text="DATA INDIVIDU").click()
-    for name, resourceId in INDIVIDU_COL.items():
+    for name, (resourceId, default) in INDIVIDU_COL.items():
         try:
+            value = d_get_text(d, resourceId)
+            if default and value == default:
+                continue
             data[name] = d_get_text(d, resourceId)
         except UiObjectNotFoundError:
             continue
