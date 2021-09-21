41909fbc795084220c1e38c144dae44c5aa8d63b
hexatester <revolusi147id@gmail.com>
2021-09-21 22:38:18 +0700

    Added rstrip

--- sdgs_tools/aplikasi_sdgs/export_keluarga/tenaga_kesehatan.py
+++ sdgs_tools/aplikasi_sdgs/export_keluarga/tenaga_kesehatan.py
@@ -13,10 +13,10 @@ AKSES_TENKES = {
 
 TENAGA_KESEHATAN_COL = {
     # "B": ("com.kemendes.survey:id/txtNIK", ""),
-    "nama": ("com.kemendes.survey:id/txtNIK", ""),
-    "jarak": ("com.kemendes.survey:id/txtNama", "Jarak : "),
-    "waktu": ("com.kemendes.survey:id/txtAlamat", "Waktu Tempuh :"),
-    "kemudahan": ("com.kemendes.survey:id/txtTelpon", "Kemudahan :"),
+    "nama": ("com.kemendes.survey:id/txtNIK", "", ""),
+    "jarak": ("com.kemendes.survey:id/txtNama", "Jarak : ", " km"),
+    "waktu": ("com.kemendes.survey:id/txtAlamat", "Waktu Tempuh :", " jam"),
+    "kemudahan": ("com.kemendes.survey:id/txtTelpon", "Kemudahan :", ""),
     # "F": ("com.kemendes.survey:id/txtStatus", ""),
 }
 
@@ -32,11 +32,12 @@ def get_data_tenaga_kesehatan(d: Device):
     )
     for survey_box in survey_boxes:
         data: Dict[str, Any] = dict()
-        for name, (resourceId, lstrp) in TENAGA_KESEHATAN_COL.items():
+        for name, (resourceId, lstrp, rstrp) in TENAGA_KESEHATAN_COL.items():
             current = survey_box.child(resourceId=resourceId)
             value: str = current.info.get("text")
             if isinstance(value, str):
-                data[name] = value.lstrip(lstrp)
+                value = value.lstrip(lstrp)
+                data[name] = value.rstrip(rstrp)
         swipe_box(d, survey_box)
         key: str = data.pop("nama")
         tenkes[AKSES_TENKES[key]] = data
